2eed00dde8dba22b1af604c7ebfb4613d0bf7e5e
ML-Duoc <ML-Duoc>
2016-05-04 20:03:29 -0300

    Added: Generacion de modulo

--- EJ1/App/WelcomeModule/controller/FormValidationController.js
+++ EJ1/App/WelcomeModule/controller/FormValidationController.js
@@ -1,3 +1,17 @@
 /**
  * Created by ML-Duoc on 02/05/2016.
  */
+
+//Generar nuestro modulo
+//Nota: [] angularJS en el arreglo
+    
+var app = angular.module('LoginFormModule', []);
+
+
+// Controller.
+
+//app = identificador q representa el modulo
+//controller = recibe 3 argumentos)
+app.controller('LoginFormController', function ($scope, $http) {
+
+});
